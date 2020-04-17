73e90cc39e2df4b3a5e14fa40036ac892a839480
Andrew Cutts <acutts@acgeospatial.co.uk>
2020-04-17 21:56:37 +0100

    updated with date added and redundant code removed
    using with statement to open and write file

--- create-bookmarks-from-readme/create_bookmarks_html.py
+++ create-bookmarks-from-readme/create_bookmarks_html.py
@@ -1,50 +1,46 @@
 import requests 
 from bs4 import BeautifulSoup, SoupStrainer
+from datetime import datetime
+today = str(datetime.today().strftime('%Y-%m-%d'))
 
 content = requests.get('https://github.com/acgeospatial/awesome-earthobservation-code/blob/master/README.md').content
-lslink =[]
+lslink = []
+flag = False
 for link in BeautifulSoup(content, parse_only=SoupStrainer('a')):
   if hasattr(link, "href"):
-    lslink.append(link['href'])
-## I've added flags on the readme.md page to know where the links reside.
-lslink_filter=[]
-flag = False
-for _, value in enumerate(lslink):
-    if value == '#start-here':
+    if (link['href']) == '#start-here':
         flag = True
-    if value == '#end':
+    if (link['href']) == '#end':
         flag = False
     else:
         pass
     if flag == True:
-        lslink_filter.append(value)
-## lskink_filter should contain all the urls and junk between start-here and end        
-ls_final = list(filter(lambda k: 'https' in k, lslink_filter)) ## remove all the non urls
+        lslink.append(link['href'])
+
+ls_final = list(filter(lambda k: 'https' in k, lslink)) ## remove all the non urls
 
 ## adapted from https://pastebin.com/wf2SgCZp
 
-_OUTPUT_FILENAME = "awesome-eo-code-bookmarks.html"
- 
-fout = open(_OUTPUT_FILENAME, 'w')
+_OUTPUT_FILENAME = today+"-awesome-eo-code-bookmarks.html"
 
-# Write the header
-fout.write("""<!DOCTYPE NETSCAPE-Bookmark-file-1>
-<!-- This is an automatically generated file.
-    It will be read and overwritten.
-    DO NOT EDIT! -->
-<META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8">
-<TITLE>Awesome-EO-code-Bookmarks</TITLE>
-<H1>EO-Bookmarks</H1>
-<DL><p>\n""")
- 
+with open(_OUTPUT_FILENAME, 'w') as f:
 
-for row in ls_final:
-        fout.write('\t<DT><A HREF="')
-        fout.write(row)
-        fout.write('">')
-        fout.write(row)
-        fout.write('</A>\n')
- 
-fout.write('</DL><p>\n')
- 
-fout.close()
+    # Write the header
+    f.write("""<!DOCTYPE NETSCAPE-Bookmark-file-1>
+    <!-- This is an automatically generated file.
+        It will be read and overwritten.
+        DO NOT EDIT! -->
+    <META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8">
+    <TITLE>Awesome-EO-code-Bookmarks</TITLE>
+    <H1>EO-Bookmarks</H1>
+    <DL><p>\n""")
+
+
+    for row in ls_final:
+            f.write('\t<DT><A HREF="')
+            f.write(row)
+            f.write('">')
+            f.write(row)
+            f.write('</A>\n')
+
+    f.write('</DL><p>\n')
